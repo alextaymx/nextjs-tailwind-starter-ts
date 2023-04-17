9d81580a6495a320fb0e79c0a653f406fe338969
Alex Tay <alextaymx@gmail.com>
2023-04-17 19:37:07 +0800

    🈲

---
+++ __tests__/app/page.test.tsx
@@ -0,0 +1,20 @@
+import Home from '@/app/page';
+import { render, screen } from '@testing-library/react';
+
+
+// The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
+// The mock has been moved to `__mocks__` folder to avoid duplication
+
+describe('Index page', () => {
+  describe('Render method', () => {
+    it('should have h2 tag', () => {
+      render(<Home />);
+
+      const heading = screen.getByRole('heading', {
+        name: /Boilerplate code/,
+      });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '@/app/globals.css';
+global
+import type { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
