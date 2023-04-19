d2d940dd4c984d6d32c214324ce871c498459cac
Alex Tay <alextaymx@gmail.com>
2023-04-19 19:27:47 +0800

    minor changes in jest

--- jest.config.js
+++ jest.config.js
@@ -24,21 +24,21 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
 
   clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    '!./src/**/_*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 30,
-      functions: 30,
-      lines: 30,
-      statements: 30,
-    },
-  },
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   './src/**/*.{js,jsx,ts,tsx}',
+  //   '!./src/**/_*.{js,jsx,ts,tsx}',
+  //   '!**/*.d.ts',
+  //   '!**/node_modules/**',
+  // ],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 30,
+  //     functions: 30,
+  //     lines: 30,
+  //     statements: 30,
+  //   },
+  // },
   testEnvironment: 'jest-environment-jsdom',
 };
 
